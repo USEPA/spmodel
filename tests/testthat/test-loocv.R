d1b2fc165c1b15ce021549d69a34e23227275ea4
Michael Dumelle <mjdumelle@gmail.com>
2022-07-21 13:57:52 -0700

    minor loocv bugfix missing data spautor

--- tests/testthat/test-loocv.R
+++ tests/testthat/test-loocv.R
@@ -1,5 +1,6 @@
 load(file = system.file("extdata", "exdata.rda", package = "spmodel"))
 load(system.file("extdata", "exdata_poly.rda", package = "spmodel"))
+load(system.file("extdata", "exdata_Mpoly.rda", package = "spmodel"))
 
 test_that("loocv works geo", {
   spmod <- splm(y ~ x, exdata, "exponential", xcoord, ycoord)
@@ -11,11 +12,15 @@ test_that("loocv works geo", {
   expect_equal(length(loocv(spmod, cv_predict = TRUE, local = TRUE)), 2)
   expect_equal(length(loocv(spmod, cv_predict = TRUE, se.fit = TRUE)), 3)
   expect_equal(length(loocv(spmod, cv_predict = TRUE, se.fit = TRUE, local = TRUE)), 3)
+  expect_equal(length(loocv(spmod, se.fit = TRUE)), 2)
+  expect_equal(length(loocv(spmod, se.fit = TRUE, local = TRUE)), 2)
   # cores 2 for cran check
   expect_equal(length(loocv(spmod, cv_predict = TRUE, local = list(parallel = TRUE, ncores = 2))), 2)
   expect_equal(length(loocv(spmod, cv_predict = TRUE, local = list(parallel = TRUE, method = "all", ncores = 2))), 2)
   expect_equal(length(loocv(spmod, cv_predict = TRUE, se.fit = TRUE, local = list(parallel = TRUE, ncores = 2))), 3)
   expect_equal(length(loocv(spmod, cv_predict = TRUE, se.fit = TRUE, local = list(parallel = TRUE, method = "all", ncores = 2))), 3)
+  expect_equal(length(loocv(spmod, se.fit = TRUE, local = list(parallel = TRUE, ncores = 2))), 2)
+  expect_equal(length(loocv(spmod, se.fit = TRUE, local = list(parallel = TRUE, method = "all", ncores = 2))), 2)
 
   # random effects
   spmod <- splm(y ~ x, exdata, "exponential", xcoord, ycoord, random = ~group)
@@ -33,9 +38,23 @@ test_that("loocv works auto", {
   # cores 2 for cran check
   expect_equal(length(loocv(spmod, cv_predict = TRUE, local = list(parallel = TRUE, ncores = 2))), 2)
   expect_equal(length(loocv(spmod, cv_predict = TRUE, se.fit = TRUE, local = list(parallel = TRUE, ncores = 2))), 3)
+  expect_equal(length(loocv(spmod, se.fit = TRUE, local = list(parallel = TRUE, ncores = 2))), 2)
 
   # random effects
   spmod <- spautor(y ~ x, exdata_poly, "car", random = ~group)
   expect_vector(loocv(spmod))
   expect_vector(loocv(spmod, local = TRUE))
+
+
+  # missing data
+  spmod <- spautor(y ~ x, exdata_Mpoly, "car")
+  expect_vector(loocv(spmod))
+  # cores 2 for cran check
+  expect_vector(loocv(spmod, local = list(parallel = TRUE, ncores = 2)))
+  expect_equal(length(loocv(spmod, cv_predict = TRUE)), 2)
+  expect_equal(length(loocv(spmod, cv_predict = TRUE, se.fit = TRUE)), 3)
+  # cores 2 for cran check
+  expect_equal(length(loocv(spmod, cv_predict = TRUE, local = list(parallel = TRUE, ncores = 2))), 2)
+  expect_equal(length(loocv(spmod, cv_predict = TRUE, se.fit = TRUE, local = list(parallel = TRUE, ncores = 2))), 3)
+  expect_equal(length(loocv(spmod, se.fit = TRUE, local = list(parallel = TRUE, ncores = 2))), 2)
 })

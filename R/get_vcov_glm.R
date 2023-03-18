187eb96ee7b94a2bd2e890e5df3ff0a9f291516b
Michael Dumelle <mjdumelle@gmail.com>
2023-03-18 11:00:54 -0700

    added warning message when model standard errors unstable

--- R/get_vcov_glm.R
+++ R/get_vcov_glm.R
@@ -1,4 +1,7 @@
 get_vcov_glm <- function(cov_betahat) {
   vcov_fixed <- cov_betahat
+  if (any(diag(vcov_fixed) < 0)) {
+    warning("Model fit potentially unstable. Consider fixing ie (via spcov_initial) at some non-zero value greater than 1e-4 and refitting the model.", call. = FALSE)
+  }
   vcov <- list(fixed = vcov_fixed)
 }

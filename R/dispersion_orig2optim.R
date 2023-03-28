0cc16da158fe393450724b811c24419cfa32a48e
Michael Dumelle <mjdumelle@gmail.com>
2023-03-27 22:49:52 -0700

    added bounds on optim scale for randcov and dispersion

--- R/dispersion_orig2optim.R
+++ R/dispersion_orig2optim.R
@@ -3,6 +3,11 @@ dispersion_orig2optim <- function(dispersion_initial) {
   names(dispersion_orig2optim_val) <- "dispersion_log"
   dispersion_orig2optim_is_known <- dispersion_initial$is_known
   names(dispersion_orig2optim_is_known) <- "dispersion_log"
+
+  # return dispersion parameter vector
+  dispersion_orig2optim_val <- ifelse(dispersion_orig2optim_val > 50 & !dispersion_orig2optim_is_known, 50, dispersion_orig2optim_val)
+  dispersion_orig2optim_val <- ifelse(dispersion_orig2optim_val < -50 & !dispersion_orig2optim_is_known, -50, dispersion_orig2optim_val)
+
   dispersion_initial_list_val <- list(
     value = dispersion_orig2optim_val,
     is_known = dispersion_orig2optim_is_known

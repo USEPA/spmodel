d6b0bc21c820757a03e6a7f256ded59cdad749a7
Michael Dumelle <Dumelle.Michael@epa.gov>
2024-08-23 09:58:13 -0700

    more updates to glances warnings

--- R/glances.R
+++ R/glances.R
@@ -9,8 +9,9 @@
 #' @param sort_by Sort by a \code{glance} statistic (i.e., the name of a column
 #'   output from \code{glance()} or the order of model input (\code{sort_by = "order"}).
 #'   The default is \code{"AICc"}.
-#' @param decreasing Should \code{sort_by} be decreasing or not? The default is \code{FALSE}.
-#'
+#' @param decreasing Whether \code{sort_by} should sort by decreasing order? The default is \code{FALSE}.
+#' @param warning Whether a warning is displayed when model comparisons violate certain rules.
+#'   The default is \code{TRUE}.
 #' @return A tibble where each row represents the output of \code{glance()} for
 #'   each fitted model object.
 #'
@@ -28,7 +29,7 @@
 #' )
 #' glances(lmod, spmod)
 #' glances(lmod, spmod, sort_by = "logLik", decreasing = TRUE)
-glances <- function(object, ..., sort_by = "AICc", decreasing = FALSE) {
+glances <- function(object, ...) {
   UseMethod("glances", object)
 }
 
@@ -36,13 +37,13 @@ glances <- function(object, ..., sort_by = "AICc", decreasing = FALSE) {
 #' @method glances splm
 #' @order 2
 #' @export
-glances.splm <- function(object, ..., sort_by = "AICc", decreasing = FALSE) {
+glances.splm <- function(object, ..., sort_by = "AICc", decreasing = FALSE, warning = TRUE) {
   model_list <- c(list(object), list(...))
   if (any(!(vapply(model_list, function(x) class(x), character(1)) %in% c("splm", "spautor")))) {
     stop("All models must be of class splm or spautor", call. = FALSE)
   }
   model_list_names <- c(as.character(as.list(substitute(list(object)))[-1]), as.character(as.list(substitute(list(...)))[-1]))
-  if (length(model_list) > 1) {
+  if (warning && length(model_list) > 1) {
     check_likstat_use(model_list)
   }
   model_glance <- lapply(model_list, function(x) glance(x))
@@ -66,7 +67,7 @@ glances.spautor <- glances.splm
 #' @method glances splm_list
 #' @order 4
 #' @export
-glances.splm_list <- function(object, ..., sort_by = "AICc", decreasing = FALSE) {
+glances.splm_list <- function(object, ..., sort_by = "AICc", decreasing = FALSE, warning = TRUE) {
   model_glance <- lapply(object, function(x) glance(x))
   model_bind <- do.call(rbind, model_glance)
   model_bind <- cbind(data.frame(model = names(model_glance), model_bind))
